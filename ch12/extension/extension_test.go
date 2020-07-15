d617527c0fee2a009ddae6ce8d6258806e2a1ab8
AltairAki <wangle0118@163.com>
2020-07-15 18:01:26 +0800

    复用"继承"

---
+++ ch12/extension/extension_test.go
@@ -0,0 +1,43 @@
+package extension
+
+import (
+	"fmt"
+	"testing"
+)
+
+/*继承 Golang不支持继承*/
+type Pet struct {
+}
+
+func (p *Pet) speak() {
+	fmt.Println("... ")
+}
+
+func (p *Pet) speakTo(host string) {
+	p.speak()
+	fmt.Println(host)
+}
+
+type Dog struct {
+	Pet
+}
+
+func (d *Dog) speak() {
+	fmt.Print("wang ")
+}
+
+/*只有完全重写了Pet的SpeakTo()才能调用到Dog的speak()*/
+func (d *Dog) speakTo(host string) {
+	d.speak()
+	fmt.Println(host)
+}
+
+func TestExtension(t *testing.T) {
+	// p := new(Pet)
+	// p.speakTo("chao")
+
+	d := new(Dog)
+	// 未重写speakTo ... host
+	// 重写speakTo wang host
+	d.speakTo("k")
+}
