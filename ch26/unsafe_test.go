91f1f1d197d612a967410ec256668f07d8d824a2
le118 <1046792312@qq.com>
2020-11-07 16:18:59 +0800

    update

--- ch26/unsafe_test.go
+++ ch26/unsafe_test.go
@@ -11,7 +11,8 @@ import (
 
 type MyInt int
 
-func TestConvert(t *testing.T) {
+func TestUnsafeConvert(t *testing.T) {
+	// 不安全的类型转换
 	i := 10
 	// f := *(*float64)(&i) // 编译时会报 cannot convert &i (type *int) to type *float64
 	f := *(*float64)(unsafe.Pointer(&i))
@@ -19,6 +20,7 @@ func TestConvert(t *testing.T) {
 	fmt.Println(unsafe.Pointer(&i)) // 0xc0000122f8
 	fmt.Println(f)                  // 5e-323
 
+	// 安全的类型转换
 	i2 := *(*int)(unsafe.Pointer(&i))
 	fmt.Println(i2) // 10
 
@@ -27,6 +29,13 @@ func TestConvert(t *testing.T) {
 	fmt.Println(b) // [1 2 3 4]
 }
 
+/*
+指针的原子操作
+
+并发读写一个共享缓存的时候，为了读写的线程安全
+
+在写的时候，写到另外一块地方，完全写完之后，用一个原子操作把我们读的那快地方和写的地方，重新指向一下。Buffer指向新写好的地方，这样再读的时候就会读新写好的内容，所以切换的时候要一个线程安全的特性，这时候可以用atomic 来做。
+*/
 func TestAtomic(t *testing.T) {
 	var shareBufPtr unsafe.Pointer
 	writeDataFn := func() {
@@ -61,5 +70,5 @@ func TestAtomic(t *testing.T) {
 			wg.Done()
 		}()
 	}
-
+	// wg.Wait()
 }
---
+++ ch27/to_int_filter.go
@@ -0,0 +1,31 @@
+package pipefilter
+
+import (
+	"errors"
+	"strconv"
+)
+
+var ToIntFilterWrongFormatError = errors.New("input data should be []string")
+
+type ToIntFilter struct {
+}
+
+func NewToIntFilter() *ToIntFilter {
+	return &ToIntFilter{}
+}
+
+func (tif *ToIntFilter) Process(data Request) (Response, error) {
+	parts, ok := data.([]string) //检查数据的格式、类型，是否可以处理
+	if !ok {
+		return nil, ToIntFilterWrongFormatError
+	}
+	ret := []int{}
+	for _, part := range parts {
+		s, err := strconv.Atoi(part)
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, s)
+	}
+	return ret, nil
+}
